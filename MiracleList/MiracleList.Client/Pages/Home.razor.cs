bc2633b96bbad4597abd230f867621c356e51fa8
Dr. Holger Schwichtenberg <no@mail>
2024-11-25 16:31:26 +0100

    DDC24

--- MiracleList/MiracleList.Client/Pages/Home.razor.cs
+++ MiracleList/MiracleList.Client/Pages/Home.razor.cs
@@ -1,51 +1,93 @@
 ﻿using System.Net;
 using ITVisions;
+using ITVisions.Blazor;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using MiracleListAPI;
 
 namespace MiracleList.Client.Pages;
 
-public partial class Home(MiracleList.AuthenticationManager am, MiracleListProxy proxy)
+public partial class Home(MiracleList.AuthenticationManager am, MiracleListProxy proxy, BlazorUtil Util)
 {
+ private PersistingComponentStateSubscription persistingSubscription;
 
  // alt:
  //[Inject]
  //public AuthenticationManager am { get; set; }
 
- protected override void OnInitialized()
+
+
+ private Task Persist()
  {
-  Util.Log(nameof(Home));
+  ApplicationState.PersistAsJson(nameof(categorySet), categorySet);
+
+  return Task.CompletedTask;
  }
 
  public List<BO.Category> categorySet { get; set; } = new();
- public List<BO.Task> taskSet { get; set; }  = new();
+ public List<BO.Task> taskSet { get; set; } = new();
  public BO.Category? category { get; set; } = null;
  public BO.Task? task { get; set; } = null;
 
  public string newCategoryName { get; set; } = "";
  public string newTaskTitle { get; set; } = "";
 
+ protected override bool ShouldRender() => shouldRender;
+ private bool shouldRender = false;
+
+ protected override void OnInitialized()
+ {
+  Util.Log(nameof(Home) + ": " + this.RendererInfo.Name);
+  //if (!this.RendererInfo.IsInteractive) shouldRender = false;
+  persistingSubscription = ApplicationState.RegisterOnPersisting(Persist);
+ }
+
  protected override async Task OnInitializedAsync()
  {
+
   bool r = await am.Login();
   if (r)
   {
    Util.Log("Das Token für alle weiteren Operationen ist: " + am.Token);
    // Liste der Kategorien laden
-   categorySet = await proxy.CategorySetAsync(am.Token);
+   //if (this.RendererInfo.IsInteractive)
+   //{
+   if (!ApplicationState.TryTakeFromJson<List<BO.Category>>(
+           nameof(categorySet), out var restoredCategorySet))
+   {
+    Util.Log(this.RendererInfo.Name + "-> Lade Kategorien...");
+    categorySet = await proxy.CategorySetAsync(am.Token);
+    shouldRender = true;
+   }
+   else
+   {
+    Util.Log(this.RendererInfo.Name + "-> Kategorien aus PersistentState..." + restoredCategorySet.Count);
+    categorySet = restoredCategorySet;
+    shouldRender = true;
+   }
+   //}
   }
  }
+
 
  public async Task ShowTaskSet(BO.Category cat)
  {
   this.category = cat;
+  this.task = null;
   taskSet = await proxy.TaskSetAsync(cat.CategoryID, am.Token);
  }
 
  public void ShowTaskDetails(BO.Task t)
  {
-  this.task = t;
+  if (t != this.task)
+  {
+   this.task = t;
+  }
+  else
+  {
+   this.task = null;
+  }
+  
  }
 
  public async Task NewCategory_KeyUp(KeyboardEventArgs args)
@@ -77,4 +119,23 @@ public partial class Home(MiracleList.AuthenticationManager am, MiracleListProxy
    }
   }
  }
+
+ public async Task ChangeTaskAsync(BO.Task t)
+ {
+  await proxy.ChangeTaskDoneAsync(t.TaskID, t.Done, am.Token);
+ }
+
+ public async Task TaskHasChanged(bool save)
+ {
+  if (save)
+  {
+   await proxy.ChangeTaskAsync(this.task, am.Token);
+  }
+  else
+  {
+   await ShowTaskSet(this.category);
+  }
+
+ }
+
 }
---
+++ MiracleList/MiracleList.Client/Pages/TaskEdit.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MiracleList.Client.Pages;
+
+public partial class TaskEdit
+{
+
+ [Parameter]
+ public BO.Task Task { get; set; }
+
+ [Parameter]
+ public EventCallback<bool> TaskHasChanged { get; set; }
+
+ public void Save()
+ {
+  TaskHasChanged.InvokeAsync(true);
+ }
+
+ public void Cancel()
+ {
+  TaskHasChanged.InvokeAsync(false);
+ }
+
+
+}
