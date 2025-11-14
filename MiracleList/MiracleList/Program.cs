99474ceccc78e0714cadd0a1f15b51d1124ff07d
Dr. Holger Schwichtenberg <no@mail>
2025-11-14 07:32:10 +0100

    fix

--- MiracleList/MiracleList/Program.cs
+++ MiracleList/MiracleList/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MiracleList.Client;
-using MiracleList.Client.Pages;
 using MiracleList.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,18 +16,18 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
- app.UseWebAssemblyDebugging();
+    app.UseWebAssemblyDebugging();
 }
 else
 {
- app.UseExceptionHandler("/Error", createScopeForErrors: true);
- // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
- app.UseHsts();
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
-app.UseStaticFiles();
+app.MapStaticAssets();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
@@ -37,5 +36,4 @@ app.MapRazorComponents<App>()
     .AddAdditionalAssemblies(typeof(MiracleList.Client._Imports).Assembly);
 
 app.Run();
-
 
