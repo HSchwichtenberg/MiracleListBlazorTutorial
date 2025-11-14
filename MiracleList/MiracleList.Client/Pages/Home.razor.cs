9aba934bfed81b1fc69b3683d7e62a70e4abda07
Dr. Holger Schwichtenberg <no@mail>
2025-11-14 11:28:36 +0100

    Aufgabe 8.1+8.2

--- MiracleList/MiracleList.Client/Pages/Home.razor.cs
+++ MiracleList/MiracleList.Client/Pages/Home.razor.cs
@@ -25,19 +25,29 @@ public partial class Home(AuthenticationManager am, MiracleListProxy proxy, Navi
         Util.Log(nameof(Home));
     }
 
+    [PersistentState]
     public List<BO.Category> CategorySet { get; set; } = new();
+    [PersistentState]
     public List<BO.Task> TaskSet { get; set; } = new();
+    [PersistentState]
     public BO.Category Category { get; set; }
+    [PersistentState]
     public BO.Task Task { get; set; }
+
+    public string NewCategoryName { get; set; }
+    public string NewTaskName { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
         Util.Log("Login", await am.Login());
         Util.Log("Token", am.Token);
 
-        CategorySet = await proxy.CategorySetAsync(am.Token);
-        if (CategorySet.Any()) await ShowTaskSet(CategorySet.First());
-        Util.Log("Category Count", CategorySet.Count);
+        if (CategorySet == null || CategorySet.Count == 0)
+        {
+            CategorySet = await proxy.CategorySetAsync(am.Token);
+            if (CategorySet.Any()) await ShowTaskSet(CategorySet.First());
+            Util.Log("Category Count", CategorySet.Count);
+        }
     }
 
     async Task ShowTaskSet(BO.Category c)
@@ -50,4 +60,36 @@ public partial class Home(AuthenticationManager am, MiracleListProxy proxy, Navi
     {
         this.Task = t;
     }
+
+    async Task NewCategory()
+    {
+        var newCategory = await proxy.CreateCategoryAsync(this.NewCategoryName, am.Token);
+        CategorySet.Add(newCategory);
+        this.NewCategoryName = "";
+    }
+
+    async Task NewTask()
+    {
+        var newTask = new BO.Task();
+        newTask.Title = this.NewTaskName;
+        newTask.CategoryID = this.Category.CategoryID;   //alternativ: newTask.Category = this.Category;
+        newTask.Due = DateTime.Now.AddDays(1);
+        await proxy.CreateTaskAsync(newTask, am.Token);
+        TaskSet.Add(newTask);
+        this.NewTaskName = "";
+
+    }
+
+    public async Task DoneChanged(BO.Task t)
+    {
+        try
+        {
+            await proxy.ChangeTaskDoneAsync(t.TaskID, t.Done, am.Token);
+        }
+        catch (Exception ex)
+        {
+            Util.Log("Fehler: " + ex.Message);
+        }
+    }
+
 }
