0c27d93528a8cbbba36bb3f1d1b12790abc2553b
Dr. Holger Schwichtenberg <no@mail>
2025-11-14 08:11:04 +0100

    Ende Sprint 1

---
+++ MiracleList/MiracleList.Client/AuthenticationManager.cs
@@ -0,0 +1,47 @@
+﻿using ITVisions.Blazor;
+using MiracleListAPI;
+
+namespace MiracleList.Client;
+
+// Primärkonstruktorparameter werden per DI befüllt!!!
+public class AuthenticationManager(MiracleListProxy proxy, BlazorUtil util)
+{
+
+    public string Token { get; set; }
+
+    public async Task<bool> Login()
+    {
+        var loginInfo = new LoginInfo();
+        loginInfo.ClientID = "88680afc-e010-43cc-9afd-37e43bd165c6";
+        loginInfo.Username = "Maxi@Mustermann.de";
+        loginInfo.Password = "geheim";
+        try
+        {
+            var loginInfoResult = await proxy.LoginAsync(loginInfo);
+
+            if (loginInfoResult != null && !String.IsNullOrEmpty(loginInfoResult.Token)
+             )
+            {
+                // erfolgreich
+                util.Log("Anmeldung ok. Token = " + loginInfoResult.Token);
+                this.Token = loginInfoResult.Token;
+                return true;
+            }
+            else
+            {
+                // nicht erfolgreich
+                util.Warn("Anmeldung nicht ok " + loginInfoResult?.Message);
+                this.Token = "";
+                return false;
+            }
+        }
+        catch (Exception ex)
+        {
+            util.Warn("Anmeldung nicht möglich " + ex.Message);
+            this.Token = "";
+            return false;
+        }
+
+    }
+
+}
--- MiracleList/MiracleList.Client/Pages/Home.razor.cs
+++ MiracleList/MiracleList.Client/Pages/Home.razor.cs
@@ -1,18 +1,53 @@
-﻿
-using ITVisions.Blazor;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.JSInterop;
+﻿using Microsoft.AspNetCore.Components;
 using MiracleListAPI;
 
 namespace MiracleList.Client.Pages;
 
 public partial class Home
 {
+    [Inject]
+    public AuthenticationManager am { get; set; }
+    [Inject]
+    public MiracleListProxy proxy { get; set; }
 
- protected override void OnInitialized()
- {
-  Util.Log(nameof(Home));
- }
+    bool IsActiveCategory(BO.Category c)
+    {
+        return Category != null && c.CategoryID == Category.CategoryID;
+    }
 
+    string? GetCategoryColor(BO.Category c)
+    {
+        return IsActiveCategory(c) ? "#E0EEFA" : null;
+    }
+
+    protected override void OnInitialized()
+    {
+        Util.Log(nameof(Home));
+    }
+
+    public List<BO.Category> CategorySet { get; set; } = new();
+    public List<BO.Task> TaskSet { get; set; } = new();
+    public BO.Category Category { get; set; }
+    public BO.Task Task { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        Util.Log("Login", await am.Login());
+        Util.Log("Token", am.Token);
+
+        CategorySet = await proxy.CategorySetAsync(am.Token);
+        if (CategorySet.Any()) await ShowTaskSet(CategorySet.First());
+        Util.Log("Category Count", CategorySet.Count);
+    }
+
+    async Task ShowTaskSet(BO.Category c)
+    {
+        this.Category = c;
+        this.TaskSet = await proxy.TaskSetAsync(c.CategoryID, am.Token);
+    }
+
+    void ShowTaskDetails(BO.Task t)
+    {
+        this.Task = t;
+    }
 }
