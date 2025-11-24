07295888f435dda5067f2a61463c061946b1d949
Dr. Holger Schwichtenberg <no@mail>
2025-11-24 15:51:29 +0100

    Mit Persistante State

--- MiracleList/MiracleList.Client/Pages/Home.razor.cs
+++ MiracleList/MiracleList.Client/Pages/Home.razor.cs
@@ -5,49 +5,59 @@ namespace MiracleList.Client.Pages;
 
 public partial class Home(AuthenticationManager am, MiracleListProxy proxy, NavigationManager nav)
 {
-    //[Inject]
-    //AuthenticationManager am { get; set; }
-    //[Inject]
-    //MiracleListProxy proxy { get; set; }
+ //[Inject]
+ //AuthenticationManager am { get; set; }
+ //[Inject]
+ //MiracleListProxy proxy { get; set; }
 
-    bool IsActiveCategory(BO.Category c)
-    {
-        return Category != null && c.CategoryID == Category.CategoryID;
-    }
+ bool IsActiveCategory(BO.Category c)
+ {
+  return Category != null && c.CategoryID == Category.CategoryID;
+ }
 
-    string? GetCategoryColor(BO.Category c)
-    {
-        return IsActiveCategory(c) ? "#E0EEFA" : null;
-    }
+ string? GetCategoryColor(BO.Category c)
+ {
+  return IsActiveCategory(c) ? "#E0EEFA" : null;
+ }
 
-    protected override void OnInitialized()
-    {
-        Util.Log(nameof(Home));
-    }
+ protected override void OnInitialized()
+ {
+  Util.Log(nameof(Home));
+ }
 
-    public List<BO.Category> CategorySet { get; set; } = new();
-    public List<BO.Task> TaskSet { get; set; } = new();
-    public BO.Category Category { get; set; }
-    public BO.Task Task { get; set; }
+ [PersistentState] // NEU in .NET 10.0
+ public List<BO.Category> CategorySet { get; set; } = new();
 
-    protected override async Task OnInitializedAsync()
-    {
-        Util.Log("Login", await am.Login());
-        Util.Log("Token", am.Token);
+ [PersistentState] // NEU in .NET 10.0
+ public List<BO.Task> TaskSet { get; set; } = new();
 
-        CategorySet = await proxy.CategorySetAsync(am.Token);
-        if (CategorySet.Any()) await ShowTaskSet(CategorySet.First());
-        Util.Log("Category Count", CategorySet.Count);
-    }
+ [PersistentState]
+ public BO.Category Category { get; set; }
 
-    async Task ShowTaskSet(BO.Category c)
-    {
-        this.Category = c;
-        this.TaskSet = await proxy.TaskSetAsync(c.CategoryID, am.Token);
-    }
+ [PersistentState]
+ public BO.Task Task { get; set; }
 
-    void ShowTaskDetails(BO.Task t)
-    {
-        this.Task = t;
-    }
+ protected override async Task OnInitializedAsync()
+ {
+  Util.Log("Login", await am.Login());
+  Util.Log("Token", am.Token);
+
+  if (CategorySet == null)
+  {
+   CategorySet = await proxy.CategorySetAsync(am.Token);
+   if (CategorySet.Any()) await ShowTaskSet(CategorySet.First());
+   Util.Log("Category Count", CategorySet.Count);
+  }
+ }
+
+ async Task ShowTaskSet(BO.Category c)
+ {
+  this.Category = c;
+  this.TaskSet = await proxy.TaskSetAsync(c.CategoryID, am.Token);
+ }
+
+ void ShowTaskDetails(BO.Task t)
+ {
+  this.Task = t;
+ }
 }
